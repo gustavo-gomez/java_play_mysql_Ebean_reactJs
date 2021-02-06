78c1b92850d8318b3a31904ae8d09c30614bae02
gustavogomez <gustavo.gomez-c@rainfocus.com>
2021-02-05 22:03:38 -0500

    add Ebean framework, in order to work with MYSQL create endPoints

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,18 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table names (
+  id                            integer auto_increment not null,
+  name                          varchar(255),
+  create_date                   datetime(6),
+  modified_date                 datetime(6),
+  constraint pk_names primary key (id)
+);
+
+
+# --- !Downs
+
+drop table if exists names;
+
